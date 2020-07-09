c9a89cf06e1cbd738de09d0a2b6ff92b5947a029
Daniel <snakefangox@gmail.com>
2020-07-09 14:07:19 +1200

    Starting work on sectors

---
+++ src/main/java/net/snakefangox/fasterthanc/universe/SectorManager.java
@@ -0,0 +1,14 @@
+package net.snakefangox.fasterthanc.universe;
+
+import net.minecraft.server.MinecraftServer;
+
+public class SectorManager {
+
+	public static void serverStart(MinecraftServer server) {
+
+	}
+
+	public static void serverStop(MinecraftServer server) {
+
+	}
+}
